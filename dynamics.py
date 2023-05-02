b127be860932c48a6b6895e5ea08c71df2be7a8c
Mitchell Ostrow <ostrow@mit.edu>
2023-05-01 23:39:41 -0400

    got training of 1d energy function working!

--- dynamics.py
+++ dynamics.py
@@ -15,7 +15,11 @@ def linear_dynamics(x_t, dt):
            [-1, 0], 
             [0, -2]
         ]) 
-
+    if isinstance(x_t,np.ndarray):
+        x_t = torch.tensor(x_t,dtype=torch.float32)
     matrix = torch.tensor(linear_transformation_matrix, dtype=torch.float32)
     x_tp1 = x_t + dt * x_t @ matrix
     return x_tp1
+
+def linear_1d(x_t,dt):
+    return x_t + dt * -x_t
