0825ae67432345d25f16f737a06263bf3593b454
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2023-05-03 21:04:56 -0700

    something's working?

--- dynamics.py
+++ dynamics.py
@@ -1,13 +1,15 @@
 import numpy as np
 import torch 
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
 
 def linear_dynamics(x_t, dt):
     """A linear dynamics function for an n-dimensional system.
-
     Args:
         x_t (torch.Tensor): The current state tensor.
         dt (float): The time step.
-
     Returns:
         torch.Tensor: The next state tensor after applying linear dynamics.
     """
@@ -21,5 +23,68 @@ def linear_dynamics(x_t, dt):
     x_tp1 = x_t + dt * x_t @ matrix
     return x_tp1
 
+
 def linear_1d(x_t,dt):
     return x_t + dt * -x_t
+
+
+def dampened_oscillator(m, c, k):
+    def func(t, state, m, c, k):
+        dim = len(c)
+        state = state.reshape(dim, 2)
+
+        x, x_dot = state[:, 0], state[:, 1]
+
+        # c = dampening coefficients
+        # k = stiffness coefficients
+        x_dot_dot = (-c*x_dot - k*x) / m
+
+        return np.column_stack((x_dot, x_dot_dot)).flatten()
+    
+    dim = len(c)
+    
+    x0 = np.array([1.0,] * dim)
+    x_dot0 = np.array([0.0,] * dim)
+    state = np.column_stack((x0, x_dot0)).flatten()
+
+    t_span = (0, 50)
+    t_eval = np.linspace(t_span[0], t_span[1], 1000)
+
+    solution = solve_ivp(func, t_span, state, args=(m, c, k), t_eval=t_eval, rtol=1e-6, atol=1e-6)
+
+    xs = []
+    for i in range(dim):
+        xs.append(solution.y[2*i])
+
+    return torch.Tensor(np.column_stack(xs)).cuda()
+
+
+def generate_batch(x, bs):
+    i = np.random.randint(0, x.shape[0] - bs + 1, size=(1,)).item()
+
+    return x[i : i + bs, :]
+
+
+def generate_negative_samples(x, angle, num_samples):
+    magnitude = torch.linalg.norm(x)
+
+    samples = []
+
+    while len(samples) < num_samples:
+        rand_vector = torch.rand(x.numel()).cuda()
+        rand_vector /= torch.linalg.norm(rand_vector)
+
+        if torch.dot(x/magnitude, rand_vector) < np.cos(np.deg2rad(angle)):
+            samples.append(rand_vector * magnitude)
+
+    return torch.stack(samples).cuda()
+
+
+def plot_3d(x):
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+ 
+    ax.plot3D(np.arange(x.shape[0]), x[:, 0], x[:, 1], alpha=1.0)
+    ax.set_xlabel("time")
+    ax.set_ylabel("dim 1")
+    ax.set_zlabel("dim 2")
