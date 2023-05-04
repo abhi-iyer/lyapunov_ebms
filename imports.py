0db10c9113a0a9971f898a90fd4deb3a63d7dd13
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2023-05-04 16:41:13 -0700

    imports

---
+++ imports.py
@@ -0,0 +1,9 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from scipy.integrate import solve_ivp
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from mpl_toolkits.mplot3d import Axes3D
+from tqdm import tqdm
